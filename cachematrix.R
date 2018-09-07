469832fb6230d5c69821011e9c65e041d2fb3a5e
jantwegener <42934578+jantwegener@users.noreply.github.com>
2018-09-07 10:06:23 +0200

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -12,50 +12,80 @@
 ## In order to test the functions of this script, it is usually sufficient to create a square matrix whose entries are filled by random variables. This works due to the following statement taken from wikipedia.org:
 ## "A square matrix that is not invertible is called singular or degenerate. A square matrix is singular if and only if its determinant is 0. Singular matrices are rare in the sense that a square matrix randomly selected from a continuous uniform distribution on its entries will almost never be singular." [https://en.wikipedia.org/wiki/Invertible_matrix]
 ##
+## The code can be tested as follows:
 ##
+### the k must be big enough to "see" the caching effect (may need to be adjusted for your PC)
+## k <- 1200
+### initialize a matrix
+## mraw <- matrix(rep(1, k*k), k, k)
+### add/sub white noise, most of the times this matrix is invertible
+## m <- matrix(sapply(mraw, function(x) x + runif(1, -2.0, 2.0)), k, k)
+## 
+## cm <- makeCacheMatrix(m)
+### compute the inverse
+## x <- cacheSolve(cm)
+### return the cached inverse, the runtime difference should be noticable
+## x <- cacheSolve(cm)
+## 
+### create a new matrix m1 as described above and set the new matrix to cm
+## mraw <- matrix(rep(1, k*k), k, k)
+## m1 <- matrix(sapply(mraw, function(x) x + runif(1, -2.0, 2.0)), k, k)
+## cm$setMatrix(m1)
+### compute the inverse since the matrix changed
+## x <- cacheSolve(cm)
+### return the cached inverse, the runtime difference should be noticable
+## x <- cacheSolve(cm)
+
+
+## This function returns a list which provides the access functions to a cache matrix object.
+## The input must be an invertible, square matrix.
+## To access the cached inverse matrix, the method cacheSolve() should be called.
 ##
-
-
-## Write a short comment describing this function
-
+## The functions in the returned list are: setMatrix, getMatrix, setInv, and getInv.
+## setMatrix: sets a new matrix to be cached
+## getMatrix: returns the currently cached matrix
+## setInv: set the inverse matrix (should not be called directly)
+## getInv: returns the currently stored inverse matrix (should not be called directly)
 makeCacheMatrix <- function(x = matrix()) {
-
-        ## m = a matrix
-        
-        # For this assignment, assume that the matrix supplied is always invertible.
-        
+        ## x = a matrix
         # the cached inverse matrix
         minv <<- NULL
+        # set the matrix
         setMatrix <- function(m_) {
-                m <<- m_
+                x <<- m_
                 minv <<- NULL
         }
+        # returns the matrix
         getMatrix <- function() {
-                m
+                x
         }
+        # sets the inverse matrix
         setInv <- function(minv_) {
                 minv <<- minv_
         }
+        # returns the inverse matrix
         getInv <- function() {
                 minv
         }
+        # the list of methods
         list(setMatrix = setMatrix, getMatrix = getMatrix, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
-
+## This method returns the inverse matrix of the given matrix.
+## When the inverse matrix has already been computed the cached value is returned, otherwise the inverse matrix is computed.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         
-        ## m = a matrix created with makeCacheMatrix
-        minv <- m$getInv()
+        ## x = a matrix created with makeCacheMatrix
+        minv <- x$getInv()
         if (is.null(minv)) {
                 print("compute")
                 # not cached so compute
-                minv <- solve(m$getMatrix(), ...)
+                minv <- solve(x$getMatrix(), ...)
                 # now we have to cache the matrix
-                m$setInv(minv)
+                x$setInv(minv)
         }
+        # return the inverse matrix
         minv
 }
